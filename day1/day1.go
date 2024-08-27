eb1bbf18effe549ac11f50d9cfef2d1be8f5afef
aokingo <annekadiso@gmail.com>
2024-08-28 02:00:22 +0300

    feat: add file reading and processing logic to main for Floor calculation

---
+++ day1/day1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	f "AoC_2015-/function"
+)
+
+func main(){
+	if len(os.Args) != 2{
+		fmt.Println("Error Usage should be : go run . lift.txt")
+		os.Exit(1)
+	}
+
+	filepath := os.Args[1]
+
+	FilePath, err := os.Open(filepath)
+	if err != nil{
+		fmt.Println("Error Opening File", err)
+		return
+	}
+	defer FilePath.Close()
+
+	scanner := bufio.NewScanner(FilePath)
+
+	for scanner.Scan(){
+		santasteps := scanner.Text()
+		fmt.Println(f.Floor(santasteps))
+	}
+}
