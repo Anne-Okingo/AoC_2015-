755c55e2308afa6b7d735175c2a8ff6e6c00a055
aokingo <annekadiso@gmail.com>
2024-08-28 23:28:59 +0300

    test : add a unit test for a function to calculate Surface Area of a data set

---
+++ function/WrapPaper_test.go
@@ -0,0 +1,16 @@
+package functions
+
+import(
+"testing"
+)
+
+func TestWrapPaper(t*testing.T){
+	l,w,h := 30,28,5
+
+	want :=2400
+
+	got := WrapPaper(l,w,h)
+	if want != got{
+		t.Errorf("TestWrapPaper Failed : want %v; got %v", want, got)
+	}
+}
