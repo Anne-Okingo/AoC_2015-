686d1733b63026ea29653147062cc61704405063
aokingo <annekadiso@gmail.com>
2024-08-28 18:36:44 +0300

    test: add unit test to check the first position when steps are-1

--- function/floor.go
+++ function/floor.go
@@ -10,6 +10,27 @@ func Floor(str string) int {
 		} else {
 			steps--
 		}
+
 	}
 	return steps
+}
+
+func Position(str string) int {
+	steps := 0
+	position := 0
+	positions := []int{}
+
+	for i, ch := range str {
+		position = i + 1
+
+		if ch == '(' {
+			steps++
+		} else {
+			steps--
+		}
+		if i < len(str) && steps == -1 {
+			positions = append(positions, position)
+		}
+	}
+	return positions[0]
 }
