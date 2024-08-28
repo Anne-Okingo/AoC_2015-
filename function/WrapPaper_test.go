09d14c819faaff49ef3c30d251303449866ec837
aokingo <annekadiso@gmail.com>
2024-08-29 01:14:15 +0300

    test: add a function

--- function/WrapPaper_test.go
+++ function/WrapPaper_test.go
@@ -1,16 +1,27 @@
 package functions
 
-import(
-"testing"
+import (
+	"testing"
 )
 
-func TestWrapPaper(t*testing.T){
-	l,w,h := 30,28,5
+func TestWrapPaper(t *testing.T) {
+	l, w, h := 30, 28, 5
 
-	want :=2400
+	want := 2400
 
-	got := WrapPaper(l,w,h)
-	if want != got{
+	got := WrapPaper(l, w, h)
+	if want != got {
 		t.Errorf("TestWrapPaper Failed : want %v; got %v", want, got)
+	}
+}
+
+func TestRibbon(t *testing.T) {
+	l, w, h := 30, 28, 5
+
+	want := 4266
+
+	got := Ribbon(l, w, h)
+	if want != got {
+		t.Errorf("TestRibbon Failed : want %v; got %v", want, got)
 	}
 }
