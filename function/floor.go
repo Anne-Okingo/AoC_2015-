ca4dc2e7e79d0ab1c4bfa36807ca708a4c214ecf
aokingo <annekadiso@gmail.com>
2024-08-28 17:07:05 +0300

    style: formating code to comply with style guide

--- function/floor.go
+++ function/floor.go
@@ -1,9 +1,5 @@
 package functions
 
-import (
-// "fmt"
-)
-
 func Floor(str string) int {
 	steps := 0
 
@@ -14,29 +10,6 @@ func Floor(str string) int {
 		} else {
 			steps--
 		}
-
 	}
-
 	return steps
-}
-
-func Itoa(n int) string {
-	sign := ""
-	result := ""
-	if n < 0 {
-		sign = "-"
-		n = -n
-	}
-	if n == 0 {
-		return "0"
-	}
-	for n != 0 {
-		start := '0'
-		for i := 0; i < (n % 10); i++ {
-			start++
-		}
-		result = string(start) + result
-		n /= 10
-	}
-	return sign + result
 }
