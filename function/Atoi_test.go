49d2a20931b8a72a52237541ae60439e2daec3a8
aokingo <annekadiso@gmail.com>
2024-08-28 23:30:25 +0300

    test: add a unit test for function that converts a string to integer

---
+++ function/Atoi_test.go
@@ -0,0 +1,24 @@
+package functions
+
+import (
+	"testing"
+)
+
+func TestAtoi(t *testing.T) {
+
+	tests := []struct{
+		input string
+		want int
+	}{
+		{"-254",-254},
+		{"254",254},
+		{"+254",254},
+	}
+
+	for _, test := range tests{
+		got := Atoi(test.input)
+		if test.want != got{
+			t.Errorf("TestAtoi Failed : Want : %v, Got : %v", test.want, got)
+		} 
+	}
+}
