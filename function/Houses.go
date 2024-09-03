e898628b051974876c627b2129e28895813cfe25
aokingo <annekadiso@gmail.com>
2024-09-03 11:21:24 +0300

    feat: add function that uses maps to return the unique house

---
+++ function/Houses.go
@@ -0,0 +1,26 @@
+package functions
+
+import(
+"fmt"
+)
+
+func Houses(s string) int {
+	mappedH := make(map[string]bool)
+
+	sx, sy := 0, 0
+
+	mappedH[fmt.Sprintf("%d,%d", sx, sy)] = true
+	for _, ch := range s {
+		if ch == '^' {
+			sy++
+		} else if ch == 'v' {
+			sy--
+		} else if ch == '>' {
+			sx++
+		} else if ch == '<' {
+			sx--
+		}
+		mappedH[fmt.Sprintf("%d,%d", sx, sy)] = true
+	}
+	return len(mappedH)
+}
