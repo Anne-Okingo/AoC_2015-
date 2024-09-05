9a1b23d9b46ca0d0dcdc9c1f8d61026ce17d9b74
aokingo <annekadiso@gmail.com>
2024-09-05 10:28:59 +0300

    style: format the code to increase readability

--- function/RoboSanta_test.go
+++ function/RoboSanta_test.go
@@ -1,16 +1,16 @@
 package functions
 
-import(
-"testing"
+import (
+	"testing"
 )
 
-func TestRoboSanta(t*testing.T){
+func TestRoboSanta(t *testing.T) {
 	path := "^^<<v<<v><>><<"
 
 	want := 7
 
-	got:=RoboSanta(path)
-	if want != got{
-		t.Errorf("Test RoboSanta Failed Want : %d, Got : %d",want, got)
+	got := RoboSanta(path)
+	if want != got {
+		t.Errorf("Test RoboSanta Failed Want : %d, Got : %d", want, got)
 	}
 }
